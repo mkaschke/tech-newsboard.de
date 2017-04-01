5ef826127312447ed37ffd76159a88b1d6ede59a
Martin Kaschke <kaschke.martin@gmail.com>
2017-04-01 15:01:22 +0200

    all sources in one container

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 // load check boxes
 loadData("menuTemplate","menu-container", "json/sources.json" );
 
+ // create container
+  var containerName = "news-container";
+  $('.content').prepend($("<div id="+ containerName + "></div>"));
+
 var i = 0; 
 // checkboxes
 $(document).on('click','[id^=someSwitchOptionPrimary]',function(){
@@ -9,33 +13,17 @@ $(document).on('click','[id^=someSwitchOptionPrimary]',function(){
   // get values from checkbox
   var url =  $(this).val();
   var checked =  $(this).is(':checked');
-  //var name = $(this).parent().parent().text().trim();
-
-  // create container
-  var containerName = "news-container";
-  var tempContainer = "news-temp";
+  var name = $(this).parent().parent().text().trim();
+  console.log(name);
   if(checked){  
-    if (i == 0){
 
-     $('.content').prepend($("<div id="+ containerName + "></div>"));
       // load data  
       loadData("newsTemplate", containerName , url );
-
-
-    }else{
-
-     $('.content').prepend($("<div id="+ tempContainer + "></div>"));
-     $('#' + tempContainer).hide();
-      // load data  
-      loadData("newsTemplate", tempContainer , url );
-      $("#" + tempContainer).appendTo("#" + containerName);
-    }
-  i++;
 
   
   }else{
     // Remove div TODO
-    $('#' + containerName).remove();
+    $('#' + name).remove();
   }
 
 });
@@ -66,7 +54,7 @@ $(document).on('click','[id^=someSwitchOptionPrimary]',function(){
     var compiledTemplate = Handlebars.compile(rawTemplate);
     var ourGeneratedHTML = compiledTemplate(Data);
     var outContainer = document.getElementById(Container);
-    outContainer.innerHTML = ourGeneratedHTML;
+     $(outContainer).prepend(ourGeneratedHTML);  
   }
 
 
